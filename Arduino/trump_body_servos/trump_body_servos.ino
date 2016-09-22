e470a756c6e8350ad7eff8af537ceec035aad6e9
barcesat <barcesat@gmail.com>
2016-09-22 18:47:17 +0300

    arduino servos sketch

---
+++ Arduino/trump_body_servos/trump_body_servos.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+// declare servos
+Servo A;
+Servo B;
+Servo C;
+Servo D;
+Servo E;
+Servo F;
+Servo G;
+
+// setup the array of servo positions
+int nextServo = 0;
+int servoAngles[] = {0,0,0,0,0,0,0};
+
+void setup() {
+ Serial.begin(115200);
+ 
+ // attach servos to their pins
+ A.attach(2);
+ B.attach(3);
+ C.attach(4);
+ D.attach(5);
+ E.attach(6);
+ F.attach(10);
+ G.attach(8);
+}
+
+void loop() {
+ if(Serial.available()){
+	int servoAngle = Serial.read();
+	servoAngles[nextServo] = servoAngle;
+	nextServo++;
+	if(nextServo > 6) nextServo = 0;
+ 
+ 
+ A.write(servoAngles[0]);
+ B.write(servoAngles[1]);
+ C.write(servoAngles[2]);
+ D.write(servoAngles[3]);
+ E.write(servoAngles[4]);
+ F.write(servoAngles[5]);
+ G.write(servoAngles[6]);
+ }
+
+}
